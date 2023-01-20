f4840b783226be792ec199269c7ba0bbcea940b4
Anastasia Yakimovska <60176102+yanastasia@users.noreply.github.com>
2023-01-20 18:19:51 +0200

    Update Event.cpp

--- Event.cpp
+++ Event.cpp
@@ -1,1 +1,25 @@
 #include "Event.h"
+#include <string>
+#include <sstream>
+Event::Event() { }
+
+Event::Event(string name, string start, string end) {
+    this->name = name;
+    this->startDate.setDate(start);
+    this->endDate.setDate(end);
+}
+
+bool Event::operator<(const Event& rhs) const {
+    if (startDate < rhs.startDate) {
+        return true;
+    }
+    else if (startDate == rhs.startDate) {
+        return name < rhs.name;
+    }
+
+    return false;
+}
+
+string Event::eventToString() const {
+    return name + "|" + startDate.dateToString() + "|" + endDate.dateToString();
+}
