cab6c52f991a335ab3c746595c9c978cf99d6a5a
Anastasia Yakimovska <60176102+yanastasia@users.noreply.github.com>
2023-01-20 23:37:08 +0200

    Update Calendar.h
    Created ShowSchedule method
    Created method to count events on a given date
    Shortened DAYNAMES and added DAYNAMESFULL

--- Calendar.h
+++ Calendar.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <map>
 #include "Date.h"
 #include "Event.h"
 using namespace std;
@@ -17,6 +19,7 @@ public:
     
     static const string MONTHNAMES[12];
     static const string DAYNAMES[7];
+    static const string DAYNAMESFULL[7];
     int monthDays(int month, int year);
 
     Date getCurrDate();
@@ -35,13 +38,19 @@ public:
 
     struct hasName; //helper for searching
 
+    Date inputMonthYear();
+
+    multimap<int, Event> vecToMM_MonthYear(int y, int m);
+
+    int eventsOnDate(Date d); //return how many events occure on a given date
+
+    void ShowSchedule();
+
     void CreateEventW(); //User creating an event
     void DeleteEvent(); //User deleting and event
-
-    void InputDate();
     int FirstDayOfMonth_Monday(int year, int month);
     int FirstDayOfMonth_Sunday(int year, int month, int day);
-    void generateCalendar(Date cDate);
+    void generateCalendar();
 
     void ChangeFirstWeekDay();
 
